758212d48f7c43e118820bce02f39281de941f8a
Aarti-Sharma05 <aartipokhrel05@gmail.com>
2025-11-07 22:55:37 +0530

    feat: User profile creation on user creation Created User profile model & added signals for user profile create handling on user creation

--- SkillSwap/api/models.py
+++ SkillSwap/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank= True, null=True)
+    location = models.TextField(max_length=100,blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.user_name
